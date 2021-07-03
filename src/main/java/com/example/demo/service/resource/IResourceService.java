d95d99c8a5eabd5d5bf5d5c6c481978fd017e360
cr-ysj <644022131@qq.com>
2021-07-03 14:13:34 +0800

    添加授权

--- src/main/java/com/example/demo/service/resource/IResourceService.java
+++ src/main/java/com/example/demo/service/resource/IResourceService.java
@@ -1,8 +1,19 @@
 package com.example.demo.service.resource;
 
+import com.example.demo.pojo.db.resource.Resource;
+import com.github.pagehelper.PageInfo;
+
 import java.util.List;
 import java.util.Map;
 
 public interface IResourceService {
     Map<String, List<String>> getAuthAndResource();
+
+    PageInfo getResourceList(int page, int limit);
+
+    void saveResource(Resource resource);
+
+    void delReources(List list);
+
+    void editResource(Resource resource);
 }
